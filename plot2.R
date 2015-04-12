acbf2fec5e5deaf12dfd8b897d190a6985121fd3
chirag <chirag@chirag.com>
2015-04-12 16:04:22 -0400

    Updated R files

--- plot2.R
+++ plot2.R
@@ -1,6 +1,7 @@
 data1 <- read.table("household_power_consumption\\household_power_consumption.txt", sep = ";", as.is = TRUE, skip=1)
-subDF1 <- subset(data1, as.Date(data1$V1, "%d/%m/%Y") >= as.Date("2007-02-01"))
-subDF1 <- subset(subDF1, as.Date(subDF1$V1, "%d/%m/%Y") <= as.Date("2007-02-02"))
+#subDF1 <- subset(data1, as.Date(data1$V1, "%d/%m/%Y") >= as.Date("2007-02-01"))
+#subDF1 <- subset(subDF1, as.Date(subDF1$V1, "%d/%m/%Y") <= as.Date("2007-02-02"))
+subDF1 <- subset(data1, as.Date(data1$V1, "%d/%m/%Y") >= as.Date("2007-02-01") & as.Date(data1$V1, "%d/%m/%Y") <= as.Date("2007-02-02"))
 
 png("plot2.png", 480, 480)
 
